42e0eafe92e49875fd425a5691386d66c4c272dd
wh <sunfortea123@gmail.com>
2025-07-11 10:35:11 +0200

    m

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input() name?: string;
   @Input({ required: true }) userId!: string;
-
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -31,5 +32,17 @@ export class TasksComponent {
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelTask() {
+    this.isAddingTask = false;
   }
 }
